042881a2470ef93e5d693acab458ea45cd9479f5
Mohamed Youssef <mu7med.youssef@gmail.com>
2021-03-02 01:25:19 +0200

    removed www domain

--- init-letsencrypt.sh
+++ init-letsencrypt.sh
@@ -5,7 +5,7 @@ if ! [ -x "$(command -v docker-compose)" ]; then
   exit 1
 fi
 
-domains=(ffcsa-staging.deckfamilyfarm.com www.ffcsa-staging.deckfamilyfarm.com)
+domains=(ffcsa-staging.deckfamilyfarm.com)
 rsa_key_size=4096
 data_path="./data/certbot"
 email="rj@rjewing.com" # Adding a valid address is strongly recommended
