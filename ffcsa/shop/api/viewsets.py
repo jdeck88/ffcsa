0c6663ef8047e0ee7f3d787b77e70acab68aaf73
Mohamed Youssef <mu7med.youssef@gmail.com>
2021-11-29 04:48:30 +0400

    products page enhancments

--- ffcsa/shop/api/viewsets.py
+++ ffcsa/shop/api/viewsets.py
@@ -1,10 +1,11 @@
 from django.shortcuts import get_object_or_404
-from django.db.models import  Q
+from django.db.models import  Q, query
 from rest_framework import viewsets
 from rest_framework import mixins
 from rest_framework.decorators import detail_route, list_route
 from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated
+from rest_framework import pagination
 
 from ffcsa.shop.utils import recalculate_cart, recalculate_product_price
 
@@ -16,9 +17,22 @@ from ffcsa.shop.models.Order import Order
 from .serializers import *
 
 
+
+class CustomPagination(pagination.PageNumberPagination):
+    def get_paginated_response(self, data):
+        return Response({
+            'count': self.page.paginator.count,
+            'num_pages': self.page.paginator.num_pages,
+            'next': self.get_next_link(),
+            'previous': self.get_previous_link(),
+            'results': data
+        })
+
+
 class ProductViewSet(viewsets.ModelViewSet):
     serializer_class = ProductSerializer
     queryset = Product.objects.filter(available=True)
+    pagination_class = CustomPagination
 
     def list(self, request):
         # note - need to add the filters manually as django filters
@@ -26,18 +40,26 @@ class ProductViewSet(viewsets.ModelViewSet):
         # required to run Mezzanine
 
         queryset = Product.objects.filter(available=True)
+        
+        # filter category
         filt_category = request.GET.get('category')
         if filt_category:
             # queryset to get the products/subproducts
             queryset = Product.objects.filter(
                 (Q(categories__title=filt_category) | Q(categories__parent__title=filt_category)) & Q(available=True)
             )
+
+        # filter query
+        query = request.GET.get('query')
+        queryset = queryset.filter(Q(title__icontains=query) | Q(categories__title__icontains=query) | Q(categories__parent__title__icontains=query))
+
         queryset = self.paginate_queryset(queryset)
         serializer = ProductDataSerializer(queryset, many=True)
 
         data = serializer.data
+        
+        # Increase products price by 5% for non-members
         if not request.user.is_authenticated():
-            # Increase products price by 5%
             for product in data:
                 product['price'] = recalculate_product_price(product['price'])
         return self.get_paginated_response(data)
